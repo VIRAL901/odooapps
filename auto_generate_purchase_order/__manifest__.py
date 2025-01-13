a99479ab4edf0ef877f790e9142d825d17d4e30b
viral007 <shingalaviral22@gmail.com>
2025-01-13 22:17:50 +0545

    [ADD] auto generate po

---
+++ auto_generate_purchase_order/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': 'Auto Generate Purchase Order',
+    'version': '16.0.1.0',
+    'summary': 'Automatically generate Purchase Order based on customer preferences at weekly, monthly, or yearly intervals.',
+    'author': 'SPD Solutions Pvt. Ltd.',
+    'license': 'LGPL-3',
+    'category': 'Purchase Order',
+    'description': """
+        Auto Generate Purchase Order
+        ===================
+        This module allows you to automatically generate Purchase Order for your customers based on their preferences. 
+        Key Features:
+        - Add a checkbox to enable Purchase Order automation per customer.
+        - Select intervals: Weekly, Monthly, or Yearly.
+        - Automatic Purchase Order generation using scheduled cron jobs.
+        - Seamless integration with Odoo Purchase.
+        """,
+    'depends': ['purchase','contacts'],
+    'images': ['static/description/banner.png'],
+    'data':[
+      'security/ir.model.access.csv',
+      'data/po_cron.xml',
+      'views/order_line_template.xml',
+      'views/res_partner.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'maintainer': 'SPD Solutions Pvt. Ltd.',
+}
