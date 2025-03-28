b8f3ef64a71e3f7af3b848888559949a02b6dad1
viral007 <shingalaviral22@gmail.com>
2025-03-28 11:05:54 +0545

    [ADD] add filter module

---
+++ spd_calendar_many2many_filter/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Many2many Filter in Task(Calendar View)',
+    'version': '18.0.0.1',
+    'category': 'Extra Tools',
+    'author': "SPD Solutions Pvt. Ltd.",
+    'summary': 'Add Many2many Filter for Assignee in Tasks Calendar View',
+    'description': """This module adds a Many2many filter in the task Calendar view. 
+        It enhances the user experience by allowing filtering tasks by assignees.""",
+    'depends': ['web', 'project'],
+    'installable': True,
+    'application': True,
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/inherit_task_calendar.xml',
+    ],
+}
