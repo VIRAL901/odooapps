c4dd24b5956929bf84d29541fd49875e7cffd1f7
viral007 <shingalaviral22@gmail.com>
2025-03-31 12:09:23 +0545

    [ADD] currency converison module

---
+++ spd_sale_invoice_currency_conversion/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Sales and Invoice Multi Currency Converison',
+    'version': '17.0',
+    'category': 'Sales',
+    'summary': 'Sales and Invoice Multi Currency Converison',
+    'description': """Sales and Invoice Multi Currency Converison""",
+    'depends': ['base','sale_management','account_accountant'],
+    'data': [
+        'views/sale_order.xml',
+        'views/account_move.xml'
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'spd_sale_invoice_currency_conversion/static/src/components/tax_totals/*',
+        ],
+    },
+    'author': 'SPD Solutions Pvt. Ltd.',
+    'company': 'SPD Solutions Pvt. Ltd.',
+    'maintainer': 'SPD Solutions Pvt. Ltd.',
+    "images": ["static/description/banner.png"],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ spd_sale_invoice_currency_conversion/static/src/components/tax_totals/tax_total_ex.js
@@ -0,0 +1,42 @@
+/** @odoo-module **/
+
+import { useService } from "@web/core/utils/hooks";
+import { TaxTotalsComponent } from "@account/components/tax_totals/tax_totals";
+import { patch } from "@web/core/utils/patch";
+import { formatMonetary } from "@web/views/fields/formatters";
+import { formatFloat } from "@web/core/utils/numbers";
+import { parseFloat } from "@web/views/fields/parsers";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
+import { registry } from "@web/core/registry";
+import { getCurrency } from "@web/core/currency";
+import {
+    Component,
+    onPatched,
+    onWillUpdateProps,
+    onWillRender,
+    toRaw,
+    useRef,
+    useState,
+} from "@odoo/owl";
+
+patch(TaxTotalsComponent.prototype, {
+
+    formatData(props) {
+        let totals = JSON.parse(JSON.stringify(toRaw(props.record.data[this.props.name])));
+        let value = this.props.record.data;
+        if (!totals) {
+            return;
+        }
+        const currencyFmtOpts = { currencyId: props.record.data.conversion_currency_id && props.record.data.conversion_currency_id[0] };
+
+        let amount_untaxed = totals.amount_untaxed;
+        let amount_tax = 0;
+        let subtotals = [];
+        this.totals = totals;
+        this.value = value;
+        this.model = props.record.resModel;
+    },
+     formatConversionMonetary(value) {
+        return formatMonetary(value, {currencyId: this.value.conversion_currency_id[0]});
+    }
+});
