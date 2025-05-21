370207345ecc8ec275e3bea0a502680c747c2780
viral007 <shingalaviral22@gmail.com>
2025-05-21 11:27:03 +0545

    rm

--- spd_screenshort_by_attendence/models/hr_attendance.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api
-
-class HrAttendanceInherit(models.Model):
-    _inherit = "hr.attendance"
-
-    check_in_image = fields.Binary("Check In Image", attachment=True)
-    check_out_image = fields.Binary("Check Out Image", attachment=True)
