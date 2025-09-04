d8d7778ac4c31b93b77ce0612f79e26c5ad41112
Viral Shingala <viral.cicindelasolutions@gmail.com>
2025-09-04 11:36:08 +0530

    [ADD] add unique lot or serial number name

---
+++ spd_unique_serial_number_enforcer/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Unique Lot/Serial Number Enforcer",
+    'version': '16.0.1.0.0',
+    'summary': "Prevents duplicate product serial/lot numbers across your inventory.",
+    'description': """
+        This module enforces strict uniqueness of all product serial and lot numbers in Odoo 18.
+        Any attempt to create or modify a serial number that already exists will be blocked,
+        ensuring complete traceability, compliance, and error-free stock management.
+        Perfect for businesses that require robust tracking for warranty, recall, or quality assurance.
+    """,
+    'category': 'Inventory',
+    'author': "SPD Solutions",
+    'maintainer': "SPD Solutions",
+    'license': 'LGPL-3',
+    'depends': ['base','stock'],
+    "images": ["static/description/banner.png"],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ spd_unique_serial_number_enforcer/models/product_serial.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api, _
+from odoo.exceptions import ValidationError
+
+class StockProductionLot(models.Model):
+    _inherit = 'stock.lot'
+
+    @api.constrains('name')
+    def _check_unique_name(self):
+        for record in self:
+            existing_lot_id = self.env['stock.lot'].search([('name', '=', record.name),
+                                                            ('id', '!=', record.id)], limit=1)
+            if existing_lot_id:
+                raise ValidationError(_(f'The Lot/Serial Number {record.name} already exists!'))
