7fcabe9b60a4362a955a0c48e5b86cf82bb4a4fb
Viral Shingala <viral.cicindelasolutions@gmail.com>
2025-09-15 14:28:51 +0530

    add new theme module

---
+++ spd_backend_theme/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name" : "Backend theme in Odoo19",
+    "version" : "19.0.0.1",
+    "category" : "theme",
+    'summary': 'Change backend colors',
+    "description": """
+    This module for changing backend colors
+    """,
+    "depends" : ['base'],
+    'assets': {
+        'web.assets_backend': [
+            'spd_backend_theme/static/src/css/backend.css',
+        ],
+    },
+    'qweb': [],
+    'author': 'SPD Solutions',
+    'company': 'SPD Solutions',
+    'maintainer': 'SPD Solutions',
+    'license':'LGPL-3',
+    "images": ["static/description/banner.png"],
+    "auto_install": False,
+    "installable": True,
+}
