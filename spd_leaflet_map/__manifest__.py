c9152310afe926c49c74f7871c8317afaef6bf18
Viral Shingala <viral.cicindelasolutions@gmail.com>
2025-12-31 23:02:17 +0530

    made some change

--- spd_leaflet_map/__manifest__.py
+++ spd_leaflet_map/__manifest__.py
@@ -1,14 +1,14 @@
 {
     "name": "Leaflet Map View (OpenStreetMap Integration)",
-    "summary": "Enhance your Odoo 17 experience with a custom 'Leaflet Map' view. Display markers using the powerful Leaflet.js library.",
+    "summary": "Enhance your Odoo  experience with a custom 'Leaflet Map' view. Display markers using the powerful Leaflet.js library.",
     "description": """
-Leaflet Map View for Odoo 17
+Leaflet Map View for Odoo 
 ===================================
-This module integrates the Leaflet.js library with Odoo 17, allowing you to add a custom map view (`leaflet_map`). The view is perfect for displaying geolocated data with markers on OpenStreetMap.
+This module integrates the Leaflet.js library with Odoo, allowing you to add a custom map view (`leaflet_map`). The view is perfect for displaying geolocated data with markers on OpenStreetMap.
     """,
-    "version": "17.0.1.0",
+    "version": "17.0.1.0.0",
     "author": "SPD Solutions",
-    "license": "AGPL-3",
+    "license": "OPL-1",
     "category": "Extra Tools",
     "depends": [
         "base",
@@ -27,5 +27,7 @@ This module integrates the Leaflet.js library with Odoo 17, allowing you to add 
     ],
     "installable": True,
     "application": False,
+    "price": 50.00,
+    "currency": "USD",
     "uninstall_hook": "uninstall_hook",
 }
